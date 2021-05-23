398f59b767df61e05adec8c2007b52ce32dbe5f7
Friedrich Schweizer <fs.schweizer@gmail.com>
2021-05-23 23:17:42 +0200

    implemented first part of stock class

--- stockmarket/stocks/stock_class.py
+++ stockmarket/stocks/stock_class.py
@@ -15,21 +15,23 @@ class Stock:
         self._file_path = '../data/' + self._symbol + '.pkl'
         self._symbol_data = None
 
+        self.logger = Logger()
+
     def load(self):
         if self._symbol_data:
-            Logger.info('Symbol data is not empty.')
+            self.logger.info('Symbol data is not empty.')
         elif path.isfile(self._file_path):
             self._symbol_data = pd.read_pickle(self._file_path)
-            Logger.info(msg='Symbol data loaded from pickle file: ' + self._file_path)
+            self.logger.info('Symbol data loaded from pickle file: ' + self._file_path)
         else:
-            Logger.error('Symbol data does not exist: ' + self._file_path)
+            self.logger.error('Symbol data does not exist: ' + self._file_path)
 
         return self._symbol_data is not None
 
     def save(self):
         file_path = './data/' + self._symbol + '.pkl'
         self._symbol_data.to_pickle(file_path)
-        Logger.info('Symbol data saved to: ' + self._file_path)
+        self.logger.info('Symbol data saved to: ' + self._file_path)
         return 0
 
     def get_data(self, start_date="1990-01-01", end_date=date.today()):
@@ -39,13 +41,13 @@ class Stock:
     def update(self):
         if self.load():
             if self._symbol_data.index[-1] < date.today():
-                Logger.info('Last day saved: ' + self._symbol_data.index[-1])
+                self.logger.info('Last day saved: ' + self._symbol_data.index[-1])
                 new_symbol_data = pdr.get_data_yahoo(self._symbol, start=self.symbol_data.index[-1], end=date.today())
                 if not new_symbol_data.empty:
                     try:
                         self._symbol_data = self._symbol_data.append(new_symbol_data[self._symbol_data.index[-1] + timedelta(days=1):], verify_integrity=True)
                     except:
-                        Logger.error('Update impossible.')
+                        self.logger.error('Update impossible.')
                         '''
                         os.remove(file_path)
                         symbol_data = getData(symbol, start_date, today)
@@ -56,6 +58,6 @@ class Stock:
                             failure_files.append(symbol)
                         '''
                 else:
-                    Logger.info('No new symbol data available or failed download.')
+                    self.logger.info('No new symbol data available or failed download.')
         else:
             self.get_data()
